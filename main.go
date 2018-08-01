4a4470328e18914b28162890b52ebef0ebfa7d35
agreene <agreene@redhat.com>
2018-08-01 14:20:50 -0400

    Check if username is available at websites listed in config.txt

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+    "flag"
 )
 
 func main() {
-    username := "username"
-    file, err := os.Open("config.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+    // Flags
+    // TODO: Add flag to define which file to read from
+    flag.Parse()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-      rawURL := scanner.Text()
-      updatedURL := strings.Replace(rawURL, "{{ USERNAME }}", username, -1)
-      fmt.Println(updatedURL)
+    // Get username
+    username := flag.Arg(0)
+    if username == "" {
+      log.Fatal("App must be provided a username")
     }
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
+	file, err := os.Open("config.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		context := strings.Fields(scanner.Text())
+		checkAvailability(username, context[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkAvailability (username string, url string){
+    updatedURL := strings.Replace(url, "{{USERNAME}}", username, -1)
+    resp, _ := http.Get(updatedURL)
+
+    availability := "unavailable"
+    if resp.StatusCode == 404 {
+        availability = "available"
     }
+    fmt.Println(username, availability, "at", updatedURL)
 }
